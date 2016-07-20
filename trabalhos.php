2a1a80d2cbc4ebc04a48b7f9a5f4f079d92d2761
Vinícius <vinicius@iguanaweb.com>
2016-07-20 16:44:36 -0300

    Arquivos

--- trabalhos.php
+++ trabalhos.php
@@ -48,7 +48,7 @@
     </div>
     <div class="col-md-3">
       <div class="hoverzoom-trabalhos"> <img src="images/trabalhos/trabalhos-04.png" alt="Inspeção e Manutenção">
-        <div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-04.png" alt=""> </div>
+         <a href="projetosCivis"><div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-04.png" alt=""> </div></a>
       </div>
       <div class="titulo"> <span>Projetos Civis e Elétricos</span> </div>
     </div>
@@ -56,25 +56,25 @@
   <div class="row row2">
     <div class="col-md-3">
     <div class="hoverzoom-trabalhos"> <img src="images/trabalhos/trabalhos-05.png" alt="Inspeção e Manutenção">
-        <div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-07.png" alt=""> </div>
+        <a href="projetoSpda"><div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-07.png" alt=""> </div></a>
         <div class="titulo"> <span>Projetos de SPDA</span> </div>
       </div>
     </div>
     <div class="col-md-3">
       <div class="hoverzoom-trabalhos"> <img src="images/trabalhos/trabalhos-06.png" alt="Inspeção e Manutenção">
-        <div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-08.png" alt=""> </div>
+        <a href="projetoFotovoltaico"><div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-08.png" alt=""> </div></a>
       </div>
       <div class="titulo"> <span>Projetos de Sistema Fotovoltaico</span> </div>
     </div>
     <div class="col-md-3">
       <div class="hoverzoom-trabalhos"> <img src="images/trabalhos/trabalhos-07.png" alt="Inspeção e Manutenção">
-        <div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-05.png" alt=""> </div>
+        <a href="regularizacao"><div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-05.png" alt=""> </div></a>
       </div>
       <div class="titulo"> <span>Regularização de Imóveis e Desmembramento</span> </div>
     </div>
     <div class="col-md-3">
       <div class="hoverzoom-trabalhos"> <img src="images/trabalhos/trabalhos-08.png" alt="Inspeção e Manutenção">
-        <div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-06.png" alt=""> </div>
+        <a href="instEletrica"><div class="retina-trabalhos"> <img src="images/trabalhos/trabalhos-icon-06.png" alt=""> </div></a>
       </div>
       <div class="titulo"> <span>Execução de Instalações Elétricas</span> </div>
     </div>            
@@ -82,21 +82,107 @@
 </div>
 <div class="row">
 	<div class="col-md-12">
-	<div id="inspPredial">
-    	<h2>Inspeção e Manutenção Predial</h2>
+	<div id="inspPredial" class="texto thumb-icone-param-01 ">
+        <h2>Inspeção e Manutenção Predial</h2>
         <p>As edificações são projetadas e pensadas para durarem um certo período de tempo que chamamos de 
-Vida útil. Este período pode ser aproveitado na sua totalidade ou pode ser reduzido drasticamente se não houver cuidados específicos com a edificação. Além de reduzir a vida útil, a falta de cuidados com os imó-
-veis podem colocar em risco a vida de seus usuários, bem como depreciar o bem.
-
-A SOLVE ENGENHARIA & CONSULTORIA conta com equipe profissional capacitada para realizar inspeções prediais a fim de orientar seus clientes da melhor forma para providenciar os reparos e ações conforme as reais necessidades do imóvel.
-
-A empresa também pode orientar administradores de condomínios, gerenciadores, síndicos e proprietários 
+Vida útil. Este período pode ser aproveitado na sua totalidade ou pode ser reduzido drasticamente se não houver cuidados específicos com a edificação. Além de reduzir a vida útil, a falta de cuidados com os imó-veis podem colocar em risco a vida de seus usuários, bem como depreciar o bem.</p>
+		<p>A <span class="sub-titulo">SOLVE ENGENHARIA & CONSULTORIA</span> conta com equipe profissional capacitada para realizar inspeções prediais a fim de orientar seus clientes da melhor forma para providenciar os reparos e ações conforme as reais necessidades do imóvel.</p>
+        <p>A empresa também pode orientar administradores de condomínios, gerenciadores, síndicos e proprietários 
 a respeito das manutenções necessárias para seus imóveis, através da elaboração do plano de manutenção do edifício, tudo isso visando a valorização e conservação adequada do imóvel.</p>
+		<p>A inspeção predial é um conjunto de exames feitos por profissional habilitado a fim de verificar a capacidade funcional da edificação, ou seja, apontar as suas condições técnicas, funcionais e de conservação, a fim de determinar as medidas preventivas e corretivas para a boa vida do imóvel.</p>
+        <p class="sub-titulo">Por que fazer inspeção predial?</p>
+        <p>A inspeção predial é um conjunto de exames feitos por profissional habilitado a fim de verificar a capacidade funcional da edificação, ou seja, apontar as suas condições técnicas, funcionais e de conservação, a fim de determinar as medidas preventivas e corretivas para a boa vida do imóvel.</p>
+        <p class="sub-titulo">Por que fazer manutenção predial?</p>
+        <p>O hábito da manutenção predial se revela uma boa atitude para os imóveis em geral, pois ela aumenta a vida útil do edifício (ou casa), podendo corrigir falhas ocultas aos leigos, evitando um desgaste acelerado 
+e altos gastos com reparos.</p>
     </div>
-	<div id="relatLaudos">conteudo 2</div>
-	<div id="planoPreve">conteudo 3</div>
-	<div id="div4">conteudo 4</di	v>
-	<div id="div5">conteudo 5</div>
+	<div id="relatLaudos" class="texto thumb-icone-param-02">
+    	<h2>Relatórios e Laudos Técnicos</h2>
+        <p>Os relatórios e laudos técnicos são os resultados provenientes de vistorias, inspeções prediais, auditorias 
+de projetos e perícias técnicas, por exemplo.</p>
+		<p>Apontam de maneira completamente imparcial a origem das anomalias das edificações, auxiliando no 
+		encontro de responsabilidades para cada fato encontrado.</p>
+        <p class="sub-titulo">Nossos materiais são compostos por:</p>
+       		<ul>
+        		<li>Contextualização da situação de cada imóvel;</li>
+                <li>Revisão bibliográfica para cada tipo de anomalia existente;</li>
+                <li>Registro fotográfico  completo;</li>
+        	</ul>
+         <p class="sub-titulo">Este serviço pode auxiliar nos seguintes pontos:</p>
+       		<ul>
+        		<li>Contextualização da situação de cada imóvel;</li>
+                <li>Revisão bibliográfica para cada tipo de anomalia existente;</li>
+                <li>Registro fotográfico  completo;</li>
+        		<li>Contextualização da situação de cada imóvel;</li>
+                <li>Revisão bibliográfica para cada tipo de anomalia existente;</li>
+                <li>Registro fotográfico  completo;</li>
+                <li>Registro fotográfico  completo;</li>                               
+        	</ul>
+            
+		<p>Continua com dúvidas? Contate-nos e peça um orçamento</p>      
+    </div>
+	<div id="planoPreve" class="texto thumb-icone-param-03 ">
+    	<h2>Plano de Prevenção contra Incêndio</h2>
+		<p>Depois de inúmeros acidentes na nossa região, a exigência do corpo de bombeiros quanto ao Plano de 
+Prevenção contra Incêndios (PPCI) cresceu consideravelmente, tornando o processo mais complexo e seguro aos usuários das edificações.</p>
+		<p>A <span class="sub-titulo">SOLVE ENGENHARIA & CONSULTORIA</span> trabalha com transparência e acompanhamento total do pro-
+cesso do PPCI a fim de facilitar a vida dos empresários e moradores de edifícios multifamiliares que contratam nosso serviço, informando constantemente sobre o andamento do processo.</p>
+		<p>Prezamos por projetos que gerem menos custos para o nosso cliente, considerando todas as estruturas locais já instaladas e otimizando as necessidades.</p>
+        <p>Continua com dúvidas? Contate-nos e peça um orçamento</p>
+    </div>
+	<div id="projetosCivis" class="texto thumb-icone-param-04 ">
+    	<h2>Projetos Civis e Elétricos</h2>
+        <p>Unindo os conhecimentos de engenharia civil e elétrica, a equipe da SOLVE ENGENHARIA & CONSUL-
+TORIA trabalha compatibilizando os projetos civis e elétricos, evitando que aconteçam problemas de 
+instalação destes sistemas.</p>
+		<p class="sub-titulo">Entre os projetos elétricos destacam-se os seguintes:</p>
+       		<ul>
+            	<li>Projeto e assessoria de instalações elétricas residenciais, comerciais e industriais;</li>
+				<li>Projeto e adequação de quadro de medidores;</li>
+				<li>Projetos de sistemas de aterramentos;</li>
+				<li>Projetos de eficientização energética;</li>
+				<li>Projetos de sistemas de grupos geradores;</li>
+				<li>Projetos luminotécnicos;</li>
+				<li>Prontuário das instalações elétricas;</li>
+        	</ul>
+		<p class="sub-titulo">Projetos civis destacam-se os seguintes:</p>
+       		<ul>
+        		<li>Projeto estrutural;</li>
+                <li>Projeto hidrossanitário;</li>
+                <li>Projeto arquitetônico;</li>
+        	</ul>
+    </div>
+	<div id="projetoSpda" class="texto thumb-icone-param-05">
+    	<h2>Projetos de SPDA</h2>
+        <p>É mundialmente constatado que o Brasil possui uma grande incidência anual de descargas atmosféricas. A <span class="sub-titulo">SOLVE ENGENHARIA & CONSULTORIA</span> não quer que você perca algum equipamento ou sofra com demais consequências que uma descarga atmosférica possa causar, por isso trabalhamos com o projeto de Sistemas de Proteção Contra Descargas Atmosféricas (SPDA) que consiste no dimensionamento de um sistema que realizará a condução da energia proveniente da descarga atmosférica até o solo de uma maneira segura aos usuários.</p>
+        <p class="sub-titulo">Projetos civis destacam-se os seguintes:</p>
+       		<ul>
+        		<li>Análise completa da edificação;</li>
+                <li>Avaliação da necessidade de SPDA de acordo com a NBR 5419/2015;</li>
+                <li>Inspeção de SPDA;</li>
+                <li>Certificação e acompanhamento da execução do sistema;</li>
+        	</ul>
+        <p>Continua com dúvidas? Contate-nos e peça um orçamento</p>
+    </div>
+    <div id="projetoFotovoltaico" class="texto thumb-icone-param-06">
+    	<h2>PROJETOS DE SISTEMA FOTOVOLTAICO</h2>
+        
+        
+        <p>Continua com dúvidas? Contate-nos e peça um orçamento</p>
+    </div>
+    <div id="regularizacao" class="texto thumb-icone-param-07">
+    	<h2>REGULARIZAÇÃO DE IMÓVEIS E DESMEMBRAMENTO</h2>
+        <p>Entendemos a importância de cuidar do nosso imóvel e isso inclui sua documentação junto aos órgãos competentes, como a prefeitura e corpo de bombeiros, por exemplo. Junto com isso, a <span class="sub-titulo">SOLVE ENGENHARIA & CONSULTORIA</span> trabalha para manter seu imóvel regularizado, bem como desmembrando de glebas (grandes terrenos) para que seus proprietários possam dar seguimento aos seus interesses de compra e venda, por exemplo.</p>
+        <p>Continua com dúvidas? Contate-nos e peça um orçamento</p>
+    </div>
+    <div id="instEletrica" class="texto thumb-icone-param-08">
+    	<h2>EXECUÇÃO DE INSTALAÇÕES ELÉTRICAS</h2>
+        <p>A execução das instalações elétricas consiste na transformação das informações contidas no projeto elétrico na montagem da instalação no local 		projetado. Esta execução é baseada em normas vigentes 
+como a NBR 5410 e NR-10, por exemplo, e é fundamental para que o usuário disfrute de todos os bene-
+fícios da instalação.</p>
+		<p>Muitos problemas em instalações elétricas ocorrem não no início de sua utilização e sim com o passar do tempo devido a sua execução de forma inadequada. A <span class="sub-titulo">SOLVE ENGENHARIA & CONSULTORIA</span> preza pela boa execução e acabamento, além de aplicar o que está dimensionado no projeto elétrico.</p>
+        <p>Continua com dúvidas? Contate-nos e peça um orçamento</p>
+    </div> 
    </div>
 </div>
 </div>
@@ -104,12 +190,15 @@ a respeito das manutenções necessárias para seus imóveis, através da elabor
 <script src="js/bootstrap.min.js"></script>
 <script>
 $(function(){
-
+	$("div#inspPredial").css("display", "none");
 	$("div#relatLaudos").css("display", "none");
 	$("div#planoPreve").css("display", "none");
-	$("div#div4").css("display", "none");
-	$("div#div5").css("display", "none");	
-    $("div#inspPredial").addClass("active");
+	$("div#projetosCivis").css("display", "none");
+	$("div#projetoSpda").css("display", "none");
+	$("div#projetoFotovoltaico").css("display", "none");
+	$("div#regularizacao").css("display","none");
+	$("div#instEletrica").css("display","none");
+	$("div#inspPredial").addClass("active");
 
     $("a").on("click", function( e ){         
       e.preventDefault();
